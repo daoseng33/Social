de9e211b9ae0193f24dcb0c656140446282c2086
daoseng33 <lc.ray2011@gmail.com>
2017-06-18 11:54:07 +0800

    [modify] custom login UI

---
+++ Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by Ray on 2017/6/18.
+//  Copyright © 2017年 Ray. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
